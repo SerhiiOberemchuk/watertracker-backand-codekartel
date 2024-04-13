fa1ae80e3157cea186195c3de49d4cee15b4f5ca
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-04-13 19:33:00 +0200

    Update waterServices.js

--- services/waterServices.js
+++ services/waterServices.js
@@ -1,36 +1,51 @@
 import Water from "../models/Water.js";
 
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
 export const getWaterToday = (filter) => Water.findOne(filter);
 
-export const updateWaterToday = (filter, data) => Water.findOneAndUpdate(filter, data);
+export const updateWaterToday = (filter, data) =>
+  Water.findOneAndUpdate(filter, data, { new: true });
+
+export const addWater = async (data) => {
+  const water = await Water.create(data);
+  return water;
+};
+
+export const deleteWater = (filter) => Water.findOneAndDelete(filter);
 
 export const getWaterMonth = async (user, date) => {
-  const firstDayOfMonth = dayjs(date).startOf('month').toISOString();
-  const lastDayOfMonth = dayjs(date).endOf('month').toISOString();
+  const firstDayOfMonth = dayjs(date).startOf("month").toISOString();
+  const lastDayOfMonth = dayjs(date).endOf("month").toISOString();
   const daysInMonth = dayjs(date).daysInMonth();
-  const currMonth = dayjs(date).format('MMMM');
+  const currMonth = dayjs(date).format("MMMM");
 
-  const oneMonthData = await Water.find({ user, date: { $gte: firstDayOfMonth, $lte: lastDayOfMonth } }).lean()
+  const oneMonthData = await Water.find({
+    user,
+    date: { $gte: firstDayOfMonth, $lte: lastDayOfMonth },
+  }).lean();
 
   const values = [];
 
   for (let day = 1; day <= daysInMonth; day++) {
-    const oneDayData = oneMonthData.filter(item => dayjs(item.date).date() === day);
+    const oneDayData = oneMonthData.filter(
+      (item) => dayjs(item.date).date() === day
+    );
     const date = `${day}, ${currMonth}`;
-    const dailyNorm = Math.max(...oneDayData.map(item => item.dailyNorm));
-    const sumOfValues = oneDayData.reduce((accumulator, item) => accumulator + item.value, 0);
-    const percentOfDailyNorm = Math.round(sumOfValues / dailyNorm * 100);
+    const dailyNorm = Math.max(...oneDayData.map((item) => item.dailyNorm));
+    const sumOfValues = oneDayData.reduce(
+      (accumulator, item) => accumulator + item.value,
+      0
+    );
+    const percentOfDailyNorm = Math.round((sumOfValues / dailyNorm) * 100);
     const recordsCount = oneDayData.length;
     values.push({
-      date,  // - Дата  в форматі - число, місяць (приклад - 5, April)
+      date, // - Дата  в форматі - число, місяць (приклад - 5, April)
       dailyNorm, // - Денна норма - кількість в літрах (приклад - 1.8 L)
       percentOfDailyNorm, // - Процент спожитої води від денної норми - кількість в процентах (приклад - 60%)
       recordsCount, //- Скільки разів були записи про споживання води - кількість спожвань (приклад - 6)
-    })
+    });
   }
 
   return values;
-}
-
+};
