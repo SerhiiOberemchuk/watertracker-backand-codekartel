536498817e51e462cea4fe5546c48882278940f4
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-04-14 13:13:05 +0200

    id water route update

--- routes/waterRouter.js
+++ routes/waterRouter.js
@@ -3,25 +3,30 @@ import authenticate from "../middlewares/authenticate.js";
 import validateBody from "../helpers/validateBody.js";
 import todayController from "../controllers/todayController.js";
 import waterControler from "../controllers/waterControler.js";
-import { addWaterSchema, updateWaterSchema, waterRateSchema } from "../schemas/waterSchemas.js";
+import {
+  addWaterSchema,
+  updateWaterSchema,
+  waterRateSchema,
+} from "../schemas/waterSchemas.js";
 import isValidId from "../middlewares/isValidId.js";
-
 
 const waterRouter = express.Router();
 
 waterRouter.use(authenticate);
 
-
 waterRouter.get("/today", todayController.getPercentOfDailyNorm);
 waterRouter.post("/add", validateBody(addWaterSchema), waterControler.addWater);
 waterRouter.patch(
-  "/:id/update",
+  "/:id",
   isValidId,
   validateBody(updateWaterSchema),
   waterControler.updateWater
 );
-waterRouter.patch("/calc", authenticate, validateBody(waterRateSchema),waterControler.waterRateCtrl)
-waterRouter.delete("/:id/delete", isValidId, waterControler.deleteWater);
-
+waterRouter.patch(
+  "/calc",
+  validateBody(waterRateSchema),
+  waterControler.waterRateCtrl
+);
+waterRouter.delete("/:id", isValidId, waterControler.deleteWater);
 
 export default waterRouter;
