c86da5fa227f7302f26b1abfdbe9a1516957b28e
Krislv4nk <kristinalev4enko89@gmail.com>
2024-04-15 17:47:14 +0200

    fixing

--- controllers/todayController.js
+++ controllers/todayController.js
@@ -1,36 +1,14 @@
 import * as waterServices from "../services/waterServices.js";
-import HttpError from "../helpers/HttpError.js";
 import {ctrWrapper} from "../helpers/ctrWrapper.js";
 
 
-const addWaterRecord = async (req, res) => {
-    const { amount } = req.body;
-    const { _id: user } = req.user;
-    const record = await waterServices.createWaterRecord(user, value);
-    res.status(201).json(record);
-};
-
-
-const getWaterRecordsToday = async (req, res) => {
-    const { _id: user } = req.user;
-    const result = await waterServices.getWaterRecordsForToday(user);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
-};
-
 const getPercentOfDailyNorm = async (req, res) => {
-const { _id: user } = req.user;
-const { dailyNorm } = req.params;
-const records = await waterServices.getWaterRecordsForToday(user);
-const totalAmount = records.reduce((total,record)=> total + record.value, 0);
-const percentOfDailyNorm = (totalAmount / dailyNorm) * 100;
-res.json({percentOfDailyNorm, records});
-};
+    const { userId, date } = req.user;
+    const dailyNorm = req.user.waterRate; 
+    const { percentOfDailyNorm, records } = await waterServices.getWaterRecordsToday(userId, date, dailyNorm);
+    res.json({ percentOfDailyNorm: percentOfDailyNorm, records: records });
+  };
 
 export default {
-    addWaterRecord: ctrWrapper(addWaterRecord),
-    getWaterRecordsToday: ctrWrapper(getWaterRecordsToday),
     getPercentOfDailyNorm: ctrWrapper(getPercentOfDailyNorm),
 }
