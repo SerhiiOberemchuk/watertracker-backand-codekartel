278ac4f8e21c3c055563ef62c6dd0893a9587eb0
Krislv4nk <kristinalev4enko89@gmail.com>
2024-04-17 08:02:13 +0200

    fixed services

--- controllers/todayController.js
+++ controllers/todayController.js
@@ -4,9 +4,9 @@ import {ctrWrapper} from "../helpers/ctrWrapper.js";
 
 const getPercentOfDailyNorm = async (req, res) => {
     const { userId, date } = req.user;
-    const dailyNorm = req.user.waterRate; 
-    const { percentOfDailyNorm, waterRecordsToday } = await waterServices.getWaterRecordsToday(userId, date, dailyNorm);
-    res.json({ percentOfDailyNorm: percentOfDailyNorm, waterRecordsToday: waterRecordsToday });
+    const {waterRate} = req.user
+    const { percentOfDailyNorm, arrayValuesOnly } = await waterServices.getWaterRecordsToday(userId, date, waterRate);
+    res.json({ percentOfDailyNorm: percentOfDailyNorm, waterRecordsToday: arrayValuesOnly });
   };
 
 export default {
