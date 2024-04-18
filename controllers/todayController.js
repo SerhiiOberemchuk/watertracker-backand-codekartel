49bab836708503b2e7de22716a0a4c904168b911
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-04-18 23:33:06 +0200

    water endpoints

--- controllers/todayController.js
+++ controllers/todayController.js
@@ -1,16 +1,17 @@
 import * as waterServices from "../services/waterServices.js";
-import {ctrWrapper} from "../helpers/ctrWrapper.js";
+import { ctrWrapper } from "../helpers/ctrWrapper.js";
+import HttpError from "../helpers/HttpError.js";
 
-
-  const getPercentOfDailyNorm = async (req, res) => {
-    const { _id: userId } = req.user;
-    const { percentOfDailyNorm, arrayValues } =
-      await waterServices.getWaterRecordsToday(userId);
-    res.json({ percentOfDailyNorm: percentOfDailyNorm,  arrayValues });
-  };
+const getPercentOfDailyNorm = async (req, res) => {
+  const { _id: userId } = req.user;
+  const { percentOfDailyNorm, arrayValues } =
+    await waterServices.getWaterRecordsToday(userId);
+  if (arrayValues.length === 0) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ percentOfDailyNorm: percentOfDailyNorm, arrayValues });
+};
 
 export default {
-    getPercentOfDailyNorm: ctrWrapper(getPercentOfDailyNorm),
-}
-
-
+  getPercentOfDailyNorm: ctrWrapper(getPercentOfDailyNorm),
+};
