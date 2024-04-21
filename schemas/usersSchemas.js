fb0494998dfc59f05ed84fdcaeb5edfd072448b6
Oksana Dziuba <dziuba131303@gmail.com>
2024-04-21 13:39:40 +0400

    edits UserInfoSchema, deletes unnecessary console.log

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -7,7 +7,6 @@ export const userSignUpAndLoginSchema = Joi.object({
 
 export const updateUserInfoSchema = Joi.object({
   email: Joi.string().email(),
-  password: Joi.string().min(8).max(64),
   oldPassword: Joi.string().min(8).max(64),
   newPassword: Joi.string().min(8).max(64),
   gender: Joi.string().valid("male", "female"),
