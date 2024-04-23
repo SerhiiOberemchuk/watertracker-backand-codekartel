8be4e9a706d304ea8b7ea94b34d6846f946e48dd
Oksana Dziuba <dziuba131303@gmail.com>
2024-04-23 12:58:52 +0400

    updates User Joi schemas

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,23 +1,36 @@
 import Joi from "joi";
+import { emailRegexp } from "../constans/user-constans.js";
 
 export const userSignUpAndLoginSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().pattern(emailRegexp).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
+  }),
   password: Joi.string().min(8).max(64).required(),
 });
 
 export const updateUserInfoSchema = Joi.object({
-  email: Joi.string().email(),
-  oldPassword: Joi.string().min(8).max(64).when("newPassword", {
-    is: Joi.exist(),
-    then: Joi.required(),
+  email: Joi.string().pattern(emailRegexp).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
   }),
+  oldPassword: Joi.string().min(8).max(64),
   newPassword: Joi.string().min(8).max(64),
   gender: Joi.string().valid("male", "female"),
   name: Joi.string().max(32),
 });
 
 export const userEmailSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().pattern(emailRegexp).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
+  }),
 });
 
 export const resetPasswordSchema = Joi.object({
