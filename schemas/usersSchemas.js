4c53c940aef0e9b232d6cfc8de126756334cf1e9
Oksana Dziuba <dziuba131303@gmail.com>
2024-04-23 10:57:27 +0400

    updates updateUserInfoSchema

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -7,7 +7,10 @@ export const userSignUpAndLoginSchema = Joi.object({
 
 export const updateUserInfoSchema = Joi.object({
   email: Joi.string().email(),
-  oldPassword: Joi.string().min(8).max(64),
+  oldPassword: Joi.string().min(8).max(64).when("newPassword", {
+    is: Joi.exist(),
+    then: Joi.required(),
+  }),
   newPassword: Joi.string().min(8).max(64),
   gender: Joi.string().valid("male", "female"),
   name: Joi.string().max(32),
